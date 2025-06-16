514a9d5785513d558363300d19093b577e7a86ce
Matej Trojak <mato.trojak@gmail.com>
2025-06-16 21:06:12 +0200

    Jotform - filter by creation datetime

--- jotform_api/utils.py
+++ jotform_api/utils.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import urllib.parse
 
 
 class JotformAPI:
@@ -7,6 +8,11 @@ class JotformAPI:
         self.base_url = base_url
         self.api_key = api_key
 
-    def get_submissions(self, form_id):
-        response = requests.get(f'{self.base_url}/form/{form_id}/submissions?apiKey={self.api_key}')
+    def get_submissions(self, form_id, filter_datetime=None):
+        suffix = ''
+        if filter_datetime is not None:
+            params = {'filter': json.dumps({'created_at:gt': filter_datetime})}
+            suffix = '&' + urllib.parse.urlencode(params)
+             
+        response = requests.get(f'{self.base_url}/form/{form_id}/submissions?apiKey={self.api_key}{suffix}')
         return json.loads(response.text)
