ee1eb7c8614fdb00813552ccf0163dee8a037987
Matej Trojak <mato.trojak@gmail.com>
2025-06-17 17:20:49 +0200

    Add Jotform API for changing dropdown options

--- jotform_api/utils.py
+++ jotform_api/utils.py
@@ -17,7 +17,7 @@ class JotformAPI:
             filter_datetime (str, optional): Datetime of the last known submission. Defaults to None, format "2025-06-15 08:28:53"
 
         Returns:
-            _type_: dict of all submissions
+            dict: dict of all submissions
         """
         suffix = ''
         if filter_datetime is not None:
@@ -26,3 +26,52 @@ class JotformAPI:
              
         response = requests.get(f'{self.base_url}/form/{form_id}/submissions?apiKey={self.api_key}{suffix}')
         return json.loads(response.text)
+    
+    def get_form_questions(self, form_id):
+        """Get list of form questions
+
+        Args:
+            form_id (str): identifier of a form
+
+        Returns:
+            dict: response containing questions
+        """
+        response = requests.get(f'{self.base_url}/form/{form_id}/questions?apiKey={self.api_key}')
+        return json.loads(response.text)
+    
+    def get_question_details(self, form_id, question_id):
+        """Get details about a single question
+
+        Args:
+            form_id (str): identifier of a form
+            question_id (str): identifier of a question
+
+        Returns:
+            dict: response containing details
+        """
+        response = requests.get(f'{self.base_url}/form/{form_id}/question/{question_id}?apiKey={self.api_key}')
+        return json.loads(response.text)
+
+    def update_dropdown_options(self, form_id, question_id, values):
+        """Update options for a dropdown question
+
+        Args:
+            form_id (str): identifier of a form
+            question_id (str): identifier of a form
+            values (str): pipe (|) separated values
+
+        Returns:
+            bool: True if request was successful
+        """
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        }
+        params = {
+            'apiKey': self.api_key,
+        }
+
+        response = requests.post(f'{self.base_url}/form/{form_id}/question/{question_id}',
+                                 params=params,
+                                 headers=headers,
+                                 data=f'question[options]={values}')
+        return response.status_code == 200
